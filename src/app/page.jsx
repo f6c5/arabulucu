623f288020ba1b64ff95f8a9ba469fe0e5ec70a0
f6c5 <cicekf256@gmail.com>
2023-07-09 10:36:59 +0300

    ...

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,11 +2,16 @@ import Carousel from "@/components/Carousel";
 import React from "react";
 
 const carouselItems = [
-  { id: 0, title: "item", image: "https://picsum.photos/900/380" },
-  { id: 1, title: "item", image: "https://picsum.photos/900/380" },
-  { id: 2, title: "item", image: "https://picsum.photos/900/380" },
-  { id: 3, title: "item", image: "https://picsum.photos/900/380" },
-  { id: 4, title: "item", image: "https://picsum.photos/900/380" },
+  { id: 0, title: "item", image: "/images/1.png" },
+  { id: 1, title: "item", image: "/images/2.png" },
+  { id: 2, title: "item", image: "/images/3.png" },
+  { id: 3, title: "item", image: "/images/4.png" },
+  { id: 4, title: "item", image: "/images/5.png" },
+  { id: 5, title: "item", image: "/images/6.png" },
+  { id: 6, title: "item", image: "/images/7.png" },
+  { id: 7, title: "item", image: "/images/8.png" },
+  { id: 8, title: "item", image: "/images/9.png" },
+  { id: 9, title: "item", image: "/images/10.png" },
 ];
 
 const page = () => {
