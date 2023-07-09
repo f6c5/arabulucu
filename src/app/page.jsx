7de2ab6ba169bb3b87451dad60a2ca18effa59e7
f6c5 <cicekf256@gmail.com>
2023-07-09 10:11:03 +0300

    ...

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,18 @@
 import Carousel from "@/components/Carousel";
 import React from "react";
 
+const carouselItems = [
+  { id: 0, title: "item", image: "https://picsum.photos/900/380" },
+  { id: 1, title: "item", image: "https://picsum.photos/900/380" },
+  { id: 2, title: "item", image: "https://picsum.photos/900/380" },
+  { id: 3, title: "item", image: "https://picsum.photos/900/380" },
+  { id: 4, title: "item", image: "https://picsum.photos/900/380" },
+];
+
 const page = () => {
   return (
     <div>
-      <Carousel />
+      <Carousel items={carouselItems} />
       <p>Ana Sayfa</p>
     </div>
   );
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+"use client";
 
-const Carousel = () => {
-  return <div>Carousel</div>;
+import React, { useEffect, useState } from "react";
+import { CiCircleChevLeft, CiCircleChevRight } from "react-icons/ci";
+
+const Carousel = ({ items }) => {
+  const [currentSlide, setCurrentSlide] = useState(items[0]);
+
+  const handleNext = () => {
+    if (currentSlide.id < items.length - 1) {
+      setCurrentSlide(items[currentSlide.id + 1]);
+    } else {
+      setCurrentSlide(items[0]);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentSlide.id > 0) {
+      setCurrentSlide(items[currentSlide.id - 1]);
+    } else {
+      setCurrentSlide(items[items.length - 1]);
+    }
+  };
+
+  return (
+    <div className="carousel">
+      <div className="carousel-inner">
+        {items.map((item) => (
+          <div
+            key={item.id}
+            className={`carousel-item ${
+              currentSlide.id > item.id
+                ? "next"
+                : currentSlide.id === item.id
+                ? "active"
+                : "prev"
+            }`}
+          >
+            <img src={item.image} alt={item.title} />
+          </div>
+        ))}
+      </div>
+      <div className="carousel-indicators">
+        {items.map((item) => (
+          <div
+            key={item.id}
+            className={`carousel-indicators-item ${
+              item.id === currentSlide.id && "active"
+            }`}
+            onClick={() => setCurrentSlide(items[item.id])}
+          />
+        ))}
+      </div>
+      <div className="carousel-icon left" onClick={handlePrev}>
+        <CiCircleChevLeft />
+      </div>
+      <div className="carousel-icon right" onClick={handleNext}>
+        <CiCircleChevRight />
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
