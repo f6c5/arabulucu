283826651d0b5067779b1f4ae8f27693bb025def
f6c5 <cicekf256@gmail.com>
2023-07-26 08:51:25 +0300

    ....

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,8 +1,24 @@
 export default function sitemap() {
-    return [
-      {
-        url: 'https://arabulucu.com',
-        lastModified: new Date(),
-      },
-    ];
-  }
+  return [
+    {
+      url: "https://arabulucu.vercel.app/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://arabulucu.vercel.app/articles",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://arabulucu.vercel.app/apply",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://arabulucu.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://arabulucu.vercel.app/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
